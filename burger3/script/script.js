6c18c6702df59edefc520ecbb50a97ef0787c07e
Anatoliy <davidovhz@yandex.ru>
2023-03-11 17:37:31 +0400

    burger3my

---
+++ burger3/script/script.js
@@ -0,0 +1,14 @@
+// let burger = document.body.querySelector('.header__burger');
+// burger.addEventListener('click', function () {
+//     document.body.querySelector('.header__burger').classList.toggle("active");
+//     document.body.querySelector('.header__menu').classList.toggle("active");
+//     document.body.classList.toggle("lock");
+// })
+
+
+let x = {
+    a: 25,
+    b: 5 + 9 * 2,
+    c:77
+}
+console.log('ffff');
